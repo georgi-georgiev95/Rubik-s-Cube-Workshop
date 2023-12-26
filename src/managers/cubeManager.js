dfabf3d653d1fa432fcea922550701e62e728b99
Georgi957 <gesh007@abv.bg>
2023-12-26 19:54:43 +0200

    Loads cubes dynamically from DB

--- src/managers/cubeManager.js
+++ src/managers/cubeManager.js
@@ -1,25 +1,8 @@
-const uniqid = require('uniqid');
 const Cube = require('../models/Cube');
 
-const cubes = [
-    {
-        id: 'am3qgn0lqmciut6',
-        name: 'My Cube',
-        description: "A test Rubik's Cube",
-        imageUrl: 'https://cdn.shopify.com/s/files/1/0270/0342/0758/files/cubes12_480x480.jpg?v=1609227223',
-        difficultyLevel: 4
-    },
-    {
-        id: 'am3qgn0lqmcifs0',
-        name: 'Rubik Classic',
-        description: "A second test Rubik's Cube",
-        imageUrl: 'https://shop.spinmaster.com/cdn/shop/products/o3rmamdfu2kartbxpul9.jpg?v=1694453034',
-        difficultyLevel: 3
-    }
-];
 
-exports.getAll = (search, from, to) => {
-	let result = cubes.slice();
+exports.getAll = async (search, from, to) => {
+    let result = await Cube.find().lean();
 
 	if (search) {
 		result = result.filter(cube => cube.name.toLowerCase().includes(search.toLowerCase()))
@@ -36,7 +19,7 @@ exports.getAll = (search, from, to) => {
 	return result
 };
 
-exports.getOne = (cubeId) => cubes.find(x => x.id == cubeId);
+exports.getOne = (cubeId) => Cube.findById(cubeId);
 
 exports.create = async (cubeData) => {
     const cube = new Cube(cubeData);

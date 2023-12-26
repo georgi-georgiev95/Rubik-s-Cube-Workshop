00ba755104b7699689b4aed95baee960d8efd505
Georgi957 <gesh007@abv.bg>
2023-12-26 11:28:32 +0200

    Add express to project

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
-const env = process.env.NODE_ENV || 'development';
+// const env = process.env.NODE_ENV || 'development';
 
-const config = require('./config/config')[env];
-const app = require('express')();
+// const config = require('./config/config')[env];
+// const app = require('express')();
 
-require('./config/express')(app);
-require('./config/routes')(app);
+// require('./config/express')(app);
+// require('./config/routes')(app);
 
-app.listen(config.port, console.log(`Listening on port ${config.port}! Now its up to you...`));
+// app.listen(config.port, console.log(`Listening on port ${config.port}! Now its up to you...`));
+
+const express = require('express');
+
+const app = express();
+const PORT = 3000;
+app.get('/', (req, res) => {
+    res.send('Hello from express!');
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port: ${PORT}`);
+})

a395b481eb5d0d60f7778048f3a6651bbbd2f857
Georgi957 <gesh007@abv.bg>
2023-12-26 15:04:57 +0200

    Add cube details page

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -9,7 +9,10 @@ router.post('/create', (req, res) => {
     const { name, description, imageUrl, difficultyLevel } = req.body;
     cubeManager.create({ name, description, imageUrl, difficultyLevel: Number(difficultyLevel) });
     res.redirect('/');
-})
-
+});
+router.get('/:cubeId/details', (req, res) => {
+    const cube = cubeManager.getOne(req.params.cubeId);
+    res.render('details', { cube });
+});
 
 module.exports = router;
--- src/managers/cubeManager.js
+++ src/managers/cubeManager.js
@@ -1,7 +1,24 @@
 const uniqid = require('uniqid');
-const cubes = [];
+const cubes = [
+    {
+        id: 'am3qgn0lqmciut6',
+        name: 'My Cube',
+        description: "A test Rubik's Cube",
+        imageUrl: 'https://cdn.shopify.com/s/files/1/0270/0342/0758/files/cubes12_480x480.jpg?v=1609227223',
+        difficultyLevel: 4
+    },
+    {
+        id: 'am3qgn0lqmcifs0',
+        name: 'Rubik Classic',
+        description: "A second test Rubik's Cube",
+        imageUrl: 'https://shop.spinmaster.com/cdn/shop/products/o3rmamdfu2kartbxpul9.jpg?v=1694453034',
+        difficultyLevel: 3
+    }
+];
 
 exports.getAll = () => cubes.slice();
+
+exports.getOne = (cubeId) => cubes.find(x => x.id == cubeId);
 
 exports.create = (cubeData) => {
     const newCube = {

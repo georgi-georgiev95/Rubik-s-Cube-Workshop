a0289fe5064f303934fd8b3a3f15d0f56c9d301d
Georgi957 <gesh007@abv.bg>
2023-12-27 12:06:54 +0200

    Attach accessory only once per cube

--- src/managers/accessoryManager.js
+++ src/managers/accessoryManager.js
@@ -3,3 +3,7 @@ const Accessory = require('../models/Accessory');
 exports.getAll = () => Accessory.find();
 
 exports.create = (accessoryData) => Accessory.create(accessoryData);
+
+exports.getUnattached = (accessoryIds) => Accessory.find({
+    _id: {$nin: accessoryIds}
+})

f032185413e3aa48c3138ec04f2ef7a42b2b5523
Georgi957 <gesh007@abv.bg>
2024-01-06 13:18:57 +0200

    Populate delete cube form with correct data

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -41,5 +41,10 @@ router.post('/:cubeId/attach-accessory', async (req, res) => {
     res.redirect(`/cubes/${cubeId}/details`);
 });
 
+router.get('/:cubeId/delete', async (req, res) => {
+    const cube = await cubeManager.getOne(req.params.cubeId);
+
+    res.render('cube/delete', {cube});
+})
 
 module.exports = router;

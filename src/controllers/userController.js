e5803cc24fa4ab7e84d89b13a615951118e75038
Georgi957 <gesh007@abv.bg>
2024-01-06 15:53:46 +0200

    Display proper navigation

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -9,19 +9,24 @@ router.post('/register', async (req, res) => {
     const { username, password, repeatPassword } = req.body;
     await userManager.register({ username, password, repeatPassword });
     res.redirect('/users/login');
-})
+});
 
 router.get('/login', (req, res) => {
     res.render('users/login')
-})
+});
 
 router.post('/login', async (req, res) => {
     const { username, password } = req.body;
 
-    const token = await userManager.login( username, password);
+    const token = await userManager.login(username, password);
 
-    res.cookie('auth', token, {httpOnly: true});
+    res.cookie('auth', token, { httpOnly: true });
 
+    res.redirect('/');
+});
+
+router.get('/logout', (req, res) => {
+    res.clearCookie('auth');
     res.redirect('/');
 })
 module.exports = router;
--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -7,9 +7,11 @@ exports.auth = async (req, res, next) => {
     if (token) {
         //validate token
         try {
-            const user = await jwt.verify(token, SECRET);
+            const decodedToken = await jwt.verify(token, SECRET);
 
-            req.user = user;
+            req.user = decodedToken;
+            res.locals.user = decodedToken;
+            res.locals.isAuthenticated = true;
             
             next();
         } catch (err) {
